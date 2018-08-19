097af694cdb65b7293e56e80f848555d2d6a0aa9
liquxan <39342295+liquxan@users.noreply.github.com>
2018-08-19 12:36:11 +0800

    Add files via upload

---
+++ 萊恩杯1403.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim x, i, y As Integer
+        x = TextBox1.Text
+        If x >= 10000 Or x <= 0 Then
+            End
+        End If
+        y = x * x + x + 41
+        For i = 2 To y - 1
+            If y Mod i = 0 Then
+                TextBox2.Text = "這是非質數"
+            Else
+                TextBox2.Text = "這是質數"
+            End If
+        Next
+    End Sub
+End Class
