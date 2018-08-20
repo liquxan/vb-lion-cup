ce15d68977d57dfd79aa5d8c7a34b9e59fa39ce5
liquxan <39342295+liquxan@users.noreply.github.com>
2018-08-20 10:22:00 +0800

    Update 萊恩杯1402.vb

--- 萊恩杯1402.vb
+++ 萊恩杯1402.vb
@@ -1,33 +1,38 @@
-﻿Public Class Form1
+Public Class Form1
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim x As Integer
+        Dim x, n, s As Integer
+        Dim str1 As String = ""
         x = TextBox1.Text
-        If x = 0 Then
+        If x > 0 And x < 10 Then
             TextBox2.Text = 50 & "元"
         End If
-        x -= 10
-        If x = 0 Then
-            TextBox2.Text = 50 & "元"
-        End If
+        Select Case x
+            Case 10 To 100
+                x *= 60
+                n = x / 10
+                n += 50
+                TextBox2.Text = n & "元"
+            Case 101 To 200
+                x -= 100
+                x *= 60
+                n = x / 15
+                n = n + 590
+                TextBox2.Text = n & "元"
+            Case 201 To 500
+                x -= 200
+                x *= 60
+                n = x / 20
+                n = n + 990
+                TextBox2.Text = n & "元"
+            Case x > 500
+                x -= 500
+                x *= 60
+                n = x / 30
+                n = n + 590 + 990 + 600
+                TextBox2.Text = n & "元"
+        End Select
 
-        If x > 10 And x < 100 Then
-            x *= 60
-            x = x / 10
-            TextBox2.Text = x + 50 & "元"
-        ElseIf x > 100 And x < 200 Then
-            x *= 60
-            x = x / 15
-            TextBox2.Text = x + 50 & "元"
-        ElseIf x > 200 And x < 500 Then
-            x *= 60
-            x = x / 20
-            TextBox2.Text = x + 50 & "元"
-        ElseIf x > 500 Then
-            x *= 60
-            x = x / 30
-            TextBox2.Text = x + 50 & "元"
-        End If
 
     End Sub
 End Class
